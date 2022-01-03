98c2a1002f06c4492c932f161c3f6722523b2f45
Nigam Mehta <fornigam@gmail.com>
2022-01-03 12:30:34 +0530

    immediate invoke function using async await
    Added proper usage of async and await while the time function declaration and function calling with implementation of  immediate invoke function calling method.

---
+++ immediate_invoke_function_async_await.js
@@ -0,0 +1,38 @@
+const fs = require("fs");
+const readOne = (fileName) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(`./data/${fileName}`, (err, res) => {
+      if (err) {
+        // throw new Error("Error occurred during file reading");
+        console.error("Error occurred during file reading");
+        reject(err);
+      }
+      console.log(`${fileName} one reading successfully`);
+      resolve();
+    });
+  });
+};
+
+const one = async () => {
+  console.log("one");
+  await readOne("read1.txt");
+};
+const two = async () => {
+  console.log("two");
+  await readOne("read2.txt");
+};
+const three = async () => {
+  console.log("three");
+  await readOne("read3.txt");
+};
+const four = async () => {
+  console.log("four");
+  await readOne("read4.txt");
+};
+
+(async () => {
+  await one();
+  await two();
+  await three();
+  await four();
+})();
